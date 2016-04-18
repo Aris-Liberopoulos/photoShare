251ba5b82a8710d76b7b7d89922353d3a2f8339d
Aris-Liberopoulos <liberopoulosa@gmail.com>
2016-04-18 18:30:16 +0300

    Add Paperclip gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'devise'
 
 gem 'simple_form'
 
+gem 'paperclip', '~> 5.0.0.beta1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,12 @@
 class Pin < ActiveRecord::Base
   validates :description, presence: true
+  validates :user_id, presence: true
+
 
   belongs_to :user
+  has_attached_file :image, styles: { thumb: 'x100', small: '300x300', medium: '600x600>', big: '1000x1000>' }
 
-  validates :user_id, presence: true
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
+                                    size: { less_than: 5.megabytes }
 end
